6a455627b5edac78872a4eafd3af54310b76e538
Shiri Rave <shuroo83@gmail.com>
2025-05-13 21:35:33 +0300

    Removing debug printings

--- app/bl/VendorFactory.java
+++ app/bl/VendorFactory.java
@@ -20,16 +20,12 @@ public class VendorFactory {
         String suffix = emailAddress.substring(suffixPosition+1,emailAddress.length());
         switch (suffix.toLowerCase()) {
             case Constants.GMAIL_SUFFIX:
-                System.out.println("**GMAIL_SUFFIX**");
                 return new GmailVendor(Constants.GMAIL_VENDOR_NAME);
             case Constants.WALLA_SUFFIX:
-                System.out.println("**WALLA_SUFFIX**");
                 return new WallaVendor(Constants.WALLA_VENDOR_NAME);
             case Constants.YAHOO_SUFFIX:
-                System.out.println("**YAHOO_SUFFIX**");
                 return new YahooVendor(Constants.YAHOO_VENDOR_NAME);
             default:
-                System.out.println("**Invalid suffix detected..**");
                 throw new NoSuchVendorException("Unknown email vendor detected: " + suffix+", Aborting");
         }
     }
